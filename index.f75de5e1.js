408ca080f559b493a2a686bd6c2b8e6f6de5e414
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-01-27 23:38:01 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    "Drink": {
+        "Wine": {},
+        "Schnaps": {}
+    },
+    "Fruit": {
+        "Red": {
+            "Cherry": {},
+            "Strawberry": {}
+        },
+        "Yellow": {
+            "Banana": {},
+            "Pineapple": {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ulElement = document.createElement("ul");
+    for(const key in data){
+        const liElement = document.createElement("li");
+        liElement.innerText = key;
+        ulElement.appendChild(liElement);
+        element.appendChild(ulElement);
+        if (data[key]) createTree(liElement, data[key]);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
---
+++ index.fd03e504.js
@@ -0,0 +1,2 @@
+!function e(n,t){let r=document.createElement("ul");for(let l in t){let a=document.createElement("li");a.innerText=l,r.appendChild(a),n.appendChild(r),t[l]&&e(a,t[l])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.fd03e504.js.map
